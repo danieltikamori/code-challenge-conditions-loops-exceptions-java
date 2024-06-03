4cc600d1198c5be0ee6560c09e0cf6cd6871ee3a
danieltikamori <tikamori.itiro@gmail.com>
2024-06-04 04:40:39 +0900

    Remove TODOs

--- bank-simulation/src/main/java/cc/tkmr/SimulacaoBancaria.java
+++ bank-simulation/src/main/java/cc/tkmr/SimulacaoBancaria.java
@@ -22,26 +22,26 @@ public class SimulacaoBancaria {
 
             switch (opcao) {
                 case 1:
-                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    // Ler o valor a ser depositado e atualizar/imprimir o saldo.
                     System.out.print("Digite o valor a ser depositado: ");
                     double valorDeposito = scanner.nextDouble();
                     saldo += valorDeposito;
-                    System.out.printf("Saldo atual: %.1f\n", saldo);
+                    System.out.printf("Saldo atual: %.1f%n", saldo);
                     break;
                 case 2:
-                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    // Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
                     System.out.print("Digite o valor a ser sacado: ");
                     double valorSaque = scanner.nextDouble();
                     if (saldo >= valorSaque) {
                         saldo -= valorSaque;
-                        System.out.printf("Saldo atual: %.1f\n", saldo);
+                        System.out.printf("Saldo atual: %.1f%n", saldo);
                     } else {
                         System.out.println("Saldo insuficiente.");
                     }
                     break;
                 case 3:
-                    // TODO: Exibir o saldo atual da conta.
-                    System.out.printf("Saldo atual: %.1f\n", saldo);
+                    // Exibir o saldo atual da conta.
+                    System.out.printf("Saldo atual: %.1f%n", saldo);
                     break;
                 case 0:
                     System.out.println("Programa encerrado.");
